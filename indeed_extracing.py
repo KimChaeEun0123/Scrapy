c0b5f0f2f35275b88016e2fcca1e28f326afe3f3
Kim Chae Eun <kce3615@naver.com>
2021-06-12 11:51:10 +0900

    confirm

--- indeed_extracing.py
+++ indeed_extracing.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 indeed_result = requests.get("https://www.indeed.com/jobs?as_and=python&limit=50").text
-# text로 해줘야 오류 안 생김!
+# text로 해줘야 오류 안 생김
 
 indeed_soup = BeautifulSoup(indeed_result, "html.parser")
 
