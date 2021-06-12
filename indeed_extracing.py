f3882068134787c79ffb39afc0bd981549b51425
Kim Chae Eun <kce3615@naver.com>
2021-06-12 14:41:59 +0900

    update

--- indeed_extracing.py
+++ indeed_extracing.py
@@ -2,20 +2,30 @@ import requests
 from bs4 import BeautifulSoup
 
 indeed_result = requests.get("https://www.indeed.com/jobs?as_and=python&limit=50").text
-# text로 해줘야 오류 안 생김
+# get()으로 source 받아온다. 
+# text로 바꿔줘야 BeautifulSoup 사용 가능. 
+# content: 바이너리 원문, json(): 딕셔너리 객체
+
 
 indeed_soup = BeautifulSoup(indeed_result, "html.parser")
+# html.parser: 파이썬에서 제공하는 html 해석기
+# BeautifulSoup(markup, "html.parser")
 
 pagination = indeed_soup.find("div", {"class":"pagination"})
+# div 태그의 class="pagination"을 *한 개* 찾는다.
 
-pages = pagination.find_all('a')
+links = pagination.find_all('a')
+# pagination에서 a 태그를 *모두* 찾는다. 
+# find_all은 리스트(Result Set) 형태로 반환
 '''
-for page in pages:
-    print(page.find("span"))
+for link in links:
+    print(link.find("span"))
 '''
-spans = []
-for page in pages:
-    spans.append(page.find("span"))
 
-spans = spans[:-1]
-print(spans)
+pages = []
+for link in links[:-1]:
+    pages.append(int(link.string))
+
+print(pages)
+max_page = pages[-1]
+print(max_page)
